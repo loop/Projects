f57a1a52e6dffa9f6c6fdcc1dd5e9f604940b16e
Yogesh Nagarur <yogeshnagarur@gmail.com>
2013-07-27 18:07:05 +0100

    Can find nth length of Pi for any value
    Note: It may take a long time for larger inputs

--- Numbers/nthpi.rb
+++ Numbers/nthpi.rb
@@ -1,15 +1,40 @@
 #!/usr/bin/env ruby
 
 # Prints out PI up to a certain number of digits.
-# There is problem at the moment due to the Math::PI constant.
 # A algorithm must be implemented to get Pi of a bigger length.
 # Params:
 # +limit+:: number of digits of Pi
-
-def pi(limit)
-  pi =  Math::PI.to_s
-  newlimit = Integer(limit) + 1
-  puts pi[0,newlimit]
+def pi(input)
+  q, r, t, k, n, l = 1, 0, 1, 1, 3, 3
+  decimal_point = nil
+  limit_value = Integer(input)
+  counter = 0
+  while counter != limit_value+1
+    if q*4+r-t < n*t
+      yield n
+      if decimal_point.nil?
+        yield '.'
+        decimal_point = '.'
+      end
+      counter += 1
+      nr = 10*(r-n*t)
+      n = ((10*(3*q+r)) / t) - 10*n
+      q *= 10
+      r = nr
+    else
+      nr = (2*q+r) * l
+      nn = (q*(7*k+2)+r*l) / (t*l)
+      q *= k
+      t *= l
+      l += 2
+      k += 1
+      n = nn
+      r = nr
+    end
+  end
+  if(counter == limit_value+1)
+    puts "\n"
+  end
 end
 
 # Checks if a string is a number.
@@ -25,13 +50,12 @@ def is_numeric?(s)
   end
 end
 
-
 valid = false
 while valid == false do
   puts "Enter the number of digits of PI you want to be shown.\n"
   input = gets.chomp
-  if is_numeric?(input) == true
+  if is_numeric?(input)
     valid = true
-    pi(input)
+    pi(input) {|digit| print digit; $stdout.flush}
   end
 end
